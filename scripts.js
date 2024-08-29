e4c0d0ab416db96184973dc7a53414dc1e6d39ea
akwrigh7 <akwrigh7@asu.edu>
2024-08-28 20:02:49 -0700

    added questions

--- scripts.js
+++ scripts.js
@@ -1,8 +1,17 @@
 // prettier-ignore
 let questions = [
-    "How old?",
-    "When?",
-    "Why?"
+    {
+        question: "How many lines do you need?",
+        answer: ["1", "2", "3", "4 or more"]
+    },
+    {
+        question: "How much data do you use typically?",
+        answer: ["Less than 5GB", "10GB", "25GB", "More than 50GB"]
+    },
+    {
+        question: "How much hotspot do you need?",
+        answer: ["None", "5GB", "10GB", "15GB or more"]
+    }
 ];
 
 let num = 0;
@@ -45,9 +54,22 @@ restartBtn.addEventListener("click", function(){
     loadQuestion(num);
 });
 
+let a1 = document.getElementById("a1");
+let a2 = document.getElementById("a2");
+let a3 = document.getElementById("a3");
+let a4 = document.getElementById("a4");
+
 let questionNum = document.getElementById("questionNum");
 let question = document.getElementById("question");
 function loadQuestion(num){
-    questionNum.innerHTML = `Question ${num + 1}`;
-    question.innerHTML = questions[num];
+    if (questions.length < num + 1){
+        questionsWrapper.style.display = "none";
+    } else {
+        questionNum.innerHTML = `Question ${num + 1}`;
+        question.innerHTML = questions[num].question;
+        a1.innerHTML = questions[num].answer[0];
+        a2.innerHTML = questions[num].answer[1];
+        a3.innerHTML = questions[num].answer[2];
+        a4.innerHTML = questions[num].answer[3];
+    }
 }
